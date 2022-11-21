56f71ed49ce50a4dbe17b7e1290386461cc7cce1
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2022-11-21 14:07:30 +0100

    Move plugin support to folder of same name

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,15 +1,5 @@
 local nnoremap = require("remaps.keymap").nnoremap
---local terminal = require("terminal").pytest_toggle
 
-
--- Plugin remaps
--- FZF plugin
 nnoremap("<f2>", ":FZF<cr>")
 nnoremap("<c-f>", ":Files<cr>", { silent = true })
 nnoremap("<leader>f", ":Rg<cr>", { silent = true })
-
--- Toggleterm
-
--- Fix Whitespace
-nnoremap("<f7>", ":FixWhitespace<cr>")
-
