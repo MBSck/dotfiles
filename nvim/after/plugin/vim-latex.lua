28860f80b5a61155a0bb2bdd03c1ce1b23828c9b
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-01-29 10:07:35 +0100

    Add auto-pairs (for cmp) and LaTeX support

---
+++ nvim/after/plugin/vim-latex.lua
@@ -0,0 +1,21 @@
+local nnoremap = require("remaps.keymap").nnoremap
+
+vim.g.vimtex_quickfix_open_on_warning = 0
+
+-- Movement
+nnoremap("<localleader>k", "m[")
+nnoremap("<localleader>j", "m]")
+nnoremap("<localleader>p", "[[")
+nnoremap("<localleader>n", "]]")
+
+-- Actions
+nnoremap("dsm", "<Plug>(vimtex-env-delete-math)")
+nnoremap("csm", "<Plug>(vimtex-env-change-math)")
+nnoremap("tsm", "<Plug>(vimtex-env-toggle-math)")
+
+-- Local Remaps
+nnoremap("<localleader><f1>", "<plug>(vimtex-env-surround-line)")
+nnoremap("<localleader><f1>", "<plug>(vimtex-env-surround-operator)")
+nnoremap("<localleader><f1>", "<plug>(vimtex-env-surround-visual)")
+nnoremap("<localleader><f2>", "<plug>(vimtex-cmd-create)")
+nnoremap("<localleader><f3>", "<plug>(vimtex-delim-add-modifiers)")
