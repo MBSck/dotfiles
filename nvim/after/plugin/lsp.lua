876008382623e0b701cd87c028babe9a1be6564c
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-01-29 10:45:02 +0100

    Finish lsp-config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,40 +1,93 @@
-local lsp = require("lsp-zero")
 local nnoremap = require("remaps.keymap").nnoremap
 local inoremap = require("remaps.keymap").inoremap
 
-lsp.preset("recommended")
 
-lsp.set_preferences({
-  suggest_lsp_servers = true,
-  setup_servers_on_start = true,
-  set_lsp_keymaps = false,
-  configure_diagnostics = true,
-  cmp_capabilities = true,
-  manage_nvim_cmp = true,
-  call_servers = 'local',
-  sign_icons = {
-    error = '✘',
-    warn = '▲',
-    hint = '⚑',
-    info = ''
-  }
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
 })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+require("mason-tool-installer").setup({
+    -- List of tools to be ensured installed
+    ensure_installed = {
+        {"lua-language-server", auto_update = true},        -- Lua LSP-server
+        {"vim-language-server", auto_update = true},        -- Vim LSP-server
+        {"python-lsp-server", auto_update = true},          -- Python LSP-server
+        {"rust_analyzer", auto_update = true},              -- Rust LSP-server
+        {"json-lsp", auto_update = true},                   -- JSON LSP-server
+        {"ltex-ls", auto_update = true},                    -- LaTeX LSP-server
+        {"taplo", auto_update = true},                      -- TOML LSP-server
+    },
+    -- if set to true this will check each tool for updates. If updates
+    -- are available the tool will be updated. This setting does not
+    -- affect :MasonToolsUpdate or :MasonToolsInstall.
+    -- Default: false
+    auto_update = false,
 
-  nnoremap("<leader>gd", function() vim.lsp.buf.definition() end, opts)
-  nnoremap("K", function() vim.lsp.buf.hover() end, opts)
-  nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  nnoremap("<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  nnoremap("<leader>gn", function() vim.diagnostic.goto_next() end, opts)
-  nnoremap("<leader>gp", function() vim.diagnostic.goto_prev() end, opts)
-  nnoremap("<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  nnoremap("<leader>rf", function() vim.lsp.buf.references() end, opts)
-  nnoremap("<leader>rn", function() vim.lsp.buf.rename() end, opts)
-  inoremap("<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
 
-lsp.nvim_workspace()
+    -- automatically install / update on startup. If set to false nothing
+    -- will happen on startup. You can use :MasonToolsInstall or
+    -- :MasonToolsUpdate to install tools and check for updates.
+    -- Default: true
+    run_on_start = true,
 
-lsp.setup()
+    -- set a delay (in ms) before the installation starts. This is only
+    -- effective if run_on_start is set to true.
+    -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+    -- Default: 0
+    start_delay = 3000, -- 3 second delay
+
+    -- Only attempt to install if 'debounce_hours' number of hours has
+    -- elapsed since the last time Neovim was started. This stores a
+    -- timestamp in a file named stdpath('data')/mason-tool-installer-debounce.
+    -- This is only relevant when you are using 'run_on_start'. It has no
+    -- effect when running manually via ':MasonToolsInstall' etc....
+    -- Default: nil
+    debounce_hours = 5, -- at least 5 hours between attempts to install/update
+})
+local lsp_capabilities = require("cmp_nvim_lsp").lsp_capabilities
+
+-- LSP Keybindings
+local lsp_attach = function(client, bufnr)
+    nnoremap("K", function() vim.lsp.buf.hover() end, opts)
+    inoremap("<C-k>", function() vim.lsp.buf.signature_help() end, opts)
+    nnoremap("<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    nnoremap("<leader>wa", function() vim.lsp.buf.add_workspace_folder() end, opts)
+    nnoremap("<leader>wr", function() vim.lsp.buf.remove_workspace_folder() end, opts)
+    nnoremap("<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+    nnoremap("<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    nnoremap("<leader>gi", function() vim.lsp.buf.implementation() end, opts)
+    nnoremap("<leader>gD", function() vim.lsp.buf.declaration() end, opts)
+    nnoremap("<leader>gd", function() vim.lsp.buf.definition() end, opts)
+    nnoremap("<leader>gn", function() vim.diagnostic.goto_next() end, opts)
+    nnoremap("<leader>gp", function() vim.diagnostic.goto_prev() end, opts)
+    nnoremap("<leader>gr", function() vim.lsp.buf.references() end, opts)
+    nnoremap("<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    nnoremap("<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    nnoremap("<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
+
+local lspconfig = require("lspconfig")
+require("mason-lspconfig").setup_handlers({
+    function(server_name)
+        lspconfig[server_name].setup({
+            on_attach = lsp_attach,
+            capabilities = lsp_capabilities,
+        })
+    end,
+})
+
+-- If the server behave weirdly exchange the lsp-config to this
+--local lspconfig = require('lspconfig')
+--local get_servers = require('mason-lspconfig').get_installed_servers
+
+--for _, server_name in ipairs(get_servers()) do
+  --lspconfig[server_name].setup({
+    --on_attach = lsp_attach,
+    --capabilities = lsp_capabilities,
+  --})
+--end
--- nvim/after/plugin/packer.lua
+++ nvim/after/plugin/packer.lua
@@ -6,7 +6,7 @@ return require("packer").startup(function (use)
     -- Packer package manager - Can manage itself
     use 'wbthomason/packer.nvim'
 
-    -- Learning plugin for vim
+    -- Learning plugin for vim. From PRIMAGEAN
     use 'ThePrimeagen/vim-be-good'
 
     -- Visual plugins
@@ -24,6 +24,12 @@ return require("packer").startup(function (use)
     use 'glepnir/dashboard-nvim' -- A fancy dashboard for nvim
     use 'itchyny/vim-highlighturl' -- URL highlighting
     use 'yuttie/comfortable-motion.vim' -- Simulates comfortable scroll motion
+    use {
+        "nvim-zh/colorful-winsep.nvim",
+        config = function ()
+            require('colorful-winsep').setup()
+        end
+    } -- Colorful window separation to see what window is active
 
     -- Navigation plugins
     use {'nvim-tree/nvim-tree.lua',
@@ -33,33 +39,44 @@ return require("packer").startup(function (use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    -- LSP, Git, Linting and more
-    use 'dense-analysis/ale' -- Syntax checking/linting
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+    -- LSP support
+    use {'neovim/nvim-lspconfig'}
+    use {'williamboman/mason.nvim'}
+    use {'williamboman/mason-lspconfig.nvim'}
+    use {'WhoIsSethDaniel/mason-tool-installer.nvim'}
 
+    -- Autocompletion
+    use {'hrsh7th/nvim-cmp'}
+    use {'hrsh7th/cmp-buffer'}
+    use {'hrsh7th/cmp-path'}
+    use {'saadparwaiz1/cmp_luasnip'}
+    use {'hrsh7th/cmp-nvim-lsp'}
+    use {'hrsh7th/cmp-nvim-lua'}
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    }
+    -- Snippets
+    use {'L3MON4D3/LuaSnip'}
+    use {'rafamadriz/friendly-snippets'}
+
+    -- Git support
     use 'airblade/vim-gitgutter' -- Git sideline support
 
+    -- Github copilot like tool
+    use {
+      'Exafunction/codeium.vim',
+      config = function ()
+        -- Change '<C-g>' here to any keycode you like.
+        vim.keymap.set('i', '<C-g>', function ()
+          return vim.fn['codeium#Accept']()
+        end, { expr = true })
+      end
+    }
+
     -- Code Utility Plugins
-    use 'jiangmiao/auto-pairs' -- Autopairs brackets
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    } -- Autopairs brackets
     use 'scrooloose/nerdcommenter' -- Autocomment function that is language specific
     use 'tmhedberg/SimpylFold' -- Better folding for coding
     use 'bronson/vim-trailing-whitespace' -- Quickly removes trailing Whitespace
@@ -75,6 +92,23 @@ return require("packer").startup(function (use)
         "iamcco/markdown-preview.nvim",
         run = function() vim.fn["mkdp#util#install"]() end,
     }) -- Markdown preview. Install without yarn or npm
+
+    -- Latex support
+    use {
+        'lervag/vimtex',
+        opt = true,
+        config = function ()
+            vim.g.vimtex_view_general_viewer = 'okular'
+            vim.g.vimtex_compiler_latexmk_engines = {
+                _ = '-xelatex'
+            }
+            vim.g.tex_comment_nospell = 1
+            vim.g.vimtex_compiler_progname = 'nvr'
+            vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
+            vim.g.vimtex_view_general_options_latexmk = '--unique'
+        end,
+        ft = 'tex'
+    }
 
     -- Debugging
     use { "puremourning/vimspector"} -- Graphical debugger
