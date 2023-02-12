c6005f0fbf65e1ee7cd28323fee0aeb1719dd991
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-02-12 21:50:14 +0100

    Readd treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,12 +3,5 @@ return {
   version = false, -- last release is way too old and doesn't work on Windows version = false,
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
-  opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      context_commentstring = { enable = true, enable_autocmd = false },
-      ensure_installed = {
-          "all",
-      },
-  },
+  config = require('setup.treesitter').setup,
 }
