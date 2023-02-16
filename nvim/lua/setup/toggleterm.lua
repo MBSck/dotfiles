ea608c5f77cd054d3ee887881e581c993037062b
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-02-16 01:01:24 +0100

    Add toggleterm back in

--- nvim/lua/setup/toggleterm.lua
+++ nvim/lua/setup/toggleterm.lua
@@ -29,74 +29,35 @@ return {
                 enabled = true,
             },
         })
-        require('setup.autocommand').term_autocmds()
-    end,
-    run_float = function(cmd)
-        require('toggleterm').exec(cmd, 1, 10, vim.fn.getcwd(), 'float')
+        -- Terminal variables
+        local Terminal = require("toggleterm.terminal").Terminal
+        local terminal = Terminal:new()
+        local lazygit = Terminal:new({
+            cmd = "lazygit",
+            dir = "git_dir",
+            direction = "float",
+            float_opts = { border = "double", },
+            -- function to run on opening the terminal
+            on_open = function(term)
+                vim.cmd("startinsert!")
+                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+            end,
+            -- function to run on closing the terminal
+            on_close = function(term)
+                vim.cmd("Closing terminal")
+            end,
+        })
 
-    -- Local variables
-    local python_package_cmd = "python3 -m "..vim.fn.fnamemodify(vim.fn.fnamemodify(vim.fn.getcwd(), ":h"), ":t").."."..vim.fn.fnamemodify(vim.fn.getcwd(), ":t").."."..vim.fn.expand("%:t:r")
+        function _terminal_toggle()
+            terminal:toggle()
+        end
 
-    -- Terminal variables
-    local Terminal = require("toggleterm.terminal").Terminal
-    local terminal_only = Terminal:new()
-    local python = Terminal:new({ cmd = "python3 "..vim.fn.expand("%, t") })
-    local python_package = Terminal:new({ cmd = python_package_cmd })
-    local rust_cargo_run = Terminal:new({ cmd = "cargo run"})
-    local pytest = Terminal:new({ cmd = "pytest "..vim.fn.expand("%, t") })
-    local lazygit = Terminal:new({
-        cmd = "lazygit",
-        dir = "git_dir",
-        direction = "float",
-        float_opts = { border = "double", },
-        -- function to run on opening the terminal
-        on_open = function(term)
-            vim.cmd("startinsert!")
-            vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
-        end,
-        -- function to run on closing the terminal
-        on_close = function(term)
-            vim.cmd("Closing terminal")
-        end,
-    })
+        function _lazygit_toggle()
+            lazygit:toggle()
+        end
 
-    -- TODO: Check all functions and retain only the needed ones
-    -- Create a function to open a neovim terminal in a small split window and run python
-    function _python_toggle()
-        python:toggle()
-    end
-
-    -- Create a function to open a neovim terminal in a small split window and run a python module in a package
-    function _python_package_toggle()
-        python_package:toggle()
-    end
-
-    -- Create a function to open a neovim terminal in a small split window and run pytest
-    function _pytest_toggle()
-        pytest:toggle()
-    end
-
-    -- Create a function to open a neovim terminal in a small split window and run pytest
-    function _rust_cargo_run_toggle()
-        rust_cargo_run:toggle()
-    end
-
-    -- Simply opens a toggle Terminal
-    function _lazygit_toggle()
-        lazygit:toggle()
-    end
-
-    -- Simply opens a toggle Terminal
-    function _terminal_toggle()
-        terminal_only:toggle()
-    end
-
-    local nnoremap = require("remaps.keymap").nnoremap
-    nnoremap("<f4>", ":w<CR><cmd>lua _pytest_toggle()<CR>", { silent = true })
-    nnoremap("<f5>", ":w <CR><cmd>lua _python_toggle()<CR>", { silent = true })
-    nnoremap("<leader><f5>", ":w <CR><cmd>lua _python_package_toggle()<CR>", { silent = true })
-    nnoremap("<f6>", ":w <CR><cmd>lua  _rust_cargo_run_toggle()<CR>", { silent = true })
-    nnoremap("<f8>", "<cmd>lua _lazygit_toggle()<CR>", { silent = true })
-    nnoremap("<f9>", "<cmd>lua _terminal_toggle()<CR>", { silent = true })
+        local nnoremap = require("remaps.keymap").nnoremap
+        nnoremap("<f2>", "<cmd>lua _terminal_toggle()<CR>", { silent = true })
+        nnoremap("<f4>", "<cmd>lua _lazygit_toggle()<CR>", { silent = true })
     end,
 }
