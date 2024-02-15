c0f81648fbf579312d7cbc4bfc17206e2e12fd14
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-02-15 13:19:16 +0100

    Update `zsh` and `lvim`

--- terminal/zsh/paths.zsh
+++ terminal/zsh/paths.zsh
@@ -32,21 +32,21 @@ XCBASE=`xcrun --show-sdk-path`
 # fi
 
 # Set IDL paths
-IDL_PACKAGE_DIR="/Applications/itt/idl71/packages"
-export IDL_PATH="/Applications/itt/idl71:/Applications/itt/idl71/lib:/Applications/itt/idl71/lib/wavelet/source"
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/astrolib/pro
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/textoidl
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/OIfits_Monnier/OI-data
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/starfinder
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/coyote
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/extra_idl_routines
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/idl_emcee/pro
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools/components
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools/framework
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools/iu_widgets
-export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools/extra
-export IDL_STARTUP=~/.idlrc
+# IDL_PACKAGE_DIR="/Applications/itt/idl71/packages"
+# export IDL_PATH="/Applications/itt/idl71:/Applications/itt/idl71/lib:/Applications/itt/idl71/lib/wavelet/source"
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/astrolib/pro
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/textoidl
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/OIfits_Monnier/OI-data
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/starfinder
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/coyote
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/extra_idl_routines
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/idl_emcee/pro
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools/components
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools/framework
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools/iu_widgets
+# export IDL_PATH=$IDL_PATH:$IDL_PACKAGE_DIR/itools/extra
+# export IDL_STARTUP=~/.idlrc
 
 
 # Set Important Path aliases
--- vim/lvim/lua/user/lsp/python.lua
+++ vim/lvim/lua/user/lsp/python.lua
@@ -15,22 +15,6 @@ M.setup = function()
   pcall(function()
     require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
   end)
-
-  -- Testing
-  require("neotest").setup({
-    adapters = {
-      require("neotest-python")({
-        -- Extra arguments for nvim-dap configuration
-        -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-        dap = {
-          justMyCode = false,
-          console = "integratedTerminal",
-        },
-        args = { "--log-level", "DEBUG", "--quiet" },
-        runner = "pytest",
-      })
-    }
-  })
 end
 
 return M
