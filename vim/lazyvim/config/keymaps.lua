7cf30a5a731d9dc3a590083e72d20def81bc8e5c
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-05-05 15:28:12 +0200

    New neovim configuration

--- vim/lazyvim/config/keymaps.lua
+++ vim/lazyvim/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Map save
+-- map({ "n", "x" }, "<leader>w", "<cmd>w<cr>", { expr = true, silent = true })
+
+-- Map escape
+map("i", "jj", "<esc>", { desc = "Escape" })
