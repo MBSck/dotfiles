bb064c418b1dca10c1e5ddea5a97db2ed6991467
Marten B. Scheuck <martenscheuck@gmail.com>
2025-01-25 01:05:29 +0100

    Remove lsp changes

--- lazyvim/plugins/lspconfig.lua
+++ lazyvim/plugins/lspconfig.lua
@@ -1,35 +1,35 @@
 return {
-  -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {
-          -- settings = {
-          --   python = {
-          --     pythonPath = vim.fn.getcwd() .. '/.venv/bin/python',
-          --   }
-          -- }
-        },
-        julials = {},
-    },
-  },
-},
+	-- add pyright to lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				-- pyright will be automatically installed with mason and loaded with lspconfig
+				pyright = {
+					-- settings = {
+					--   python = {
+					--     pythonPath = vim.fn.getcwd() .. '/.venv/bin/python',
+					--   }
+					-- }
+				},
+				julials = {},
+			},
+		},
+	},
 
-  -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "black",
-        "julia-lsp",
-      },
-    },
-  },
+	-- add any tools you want to have installed below
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"shellcheck",
+				"shfmt",
+				"black",
+				"julia-lsp",
+			},
+		},
+	},
 }
