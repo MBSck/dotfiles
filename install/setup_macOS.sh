90f45c212abd1b98f5fe52de28b8ce905dedce5c
Marten B. Scheuck <martenscheuck@gmail.com>
2025-01-26 20:25:35 +0100

    Update `mac` install

--- install/setup_macOS.sh
+++ install/setup_macOS.sh
@@ -9,8 +9,8 @@ dotfile_dir="$(dirname "$(dirname "$(realpath "$0")")")"
 # TODO: Rewrite this so it installs the right things
 # command_install "npm install neovim" nvim
 # command_install "gem install neovim" nvim
-install "/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'" brew
-install "curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin" kitty
+# install "/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'" brew
+# install "curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin" kitty
 install brew neovim nvim
 install brew fzf
 install brew fd
@@ -25,12 +25,12 @@ install brew uv
 
 # >>> Configuration >>>
 # TODO: Finish this setup here and move it to the end
-if [[ "$SHELL" != "/bin/zsh" ]]; then
-  echo "Changing shell to zsh..."
-  chsh -s /bin/zsh
-else
-  echo "Shell is already zsh."
-fi
+# if [[ "$SHELL" != "/bin/zsh" ]]; then
+#   echo "Changing shell to zsh..."
+#   chsh -s /bin/zsh
+# else
+#   echo "Shell is already zsh."
+# fi
 
 remove_and_relink $dotfile_dir/git/gitconfig $HOME/.gitconfig;
 remove_and_relink $dotfile_dir/zsh $HOME/zsh;
@@ -39,6 +39,7 @@ remove_and_relink $dotfile_dir/direnv $HOME/.config/direnv;
 remove_and_relink $dotfile_dir/kitty $HOME/.config/kitty;
 remove_and_relink $dotfile_dir/btop $HOME/.config/btop;
 remove_and_relink $dotfile_dir/bat $HOME/.config/bat;
+remove_and_relink $dotfile_dir/lazygit /Library/Application\ Support/lazygit
 
 # if prompt_user "Do you want to back up existing Neovim configuration files and reinstall?"; then
 #   echo "Setting up lazyvim..."
