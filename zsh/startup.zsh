2e234c51ca87dbf4fe1ff93ef33bc43ea59b98c7
Marten B. Scheuck <martenscheuck@gmail.com>
2025-01-27 11:51:01 +0100

    Update `zsh`

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -1,5 +1,10 @@
+# Set up prezto
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
 
+# Set up direnv
 eval "$(direnv hook zsh)"
+
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
