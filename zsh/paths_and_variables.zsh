112cd368348bf3c40d08d9e7e82e6d1d4343f4a3
Marten B. Scheuck <martenscheuck@gmail.com>
2025-02-04 00:31:13 +0100

    Add `IDL` config

--- zsh/paths_and_variables.zsh
+++ zsh/paths_and_variables.zsh
@@ -28,6 +28,24 @@ path=('/Users/scheuck/.juliaup/bin' $path)
 export PATH
 # <<< juliaup initialize <<<
 
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=$HOME/.config}"
-[[ -d $XDG_CONFIG_HOME/zsh ]] && export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
-source "$ZDOTDIR/.zshenv"
+# >>> IDL directories >>>
+export IDL_DIR="/Applications/NV5/idl90/"
+export IDL_PACKAGE_DIR="${IDL_DIR}/packages"
+export IDL_PATH=$IDL_PATH:"${IDL_DIR}/lib"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/astrolib/pro"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/OIfits_Monnier/OI-data"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/starfinder"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/coyote"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/extra_idl_routines"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/idl_emcee/pro"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/itools"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/itools/components"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/itools/framework"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/itools/iu_widgets"
+export IDL_PATH=$IDL_PATH:"${IDL_PACKAGE_DIR}/itools/extra"
+export IDL_STARTUP="${HOME}/.idlrc"
+# <<< IDL directories <<<
+
+# export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=$HOME/.config}"
+# [[ -d $XDG_CONFIG_HOME/zsh ]] && export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+# source "$ZDOTDIR/.zshenv"
